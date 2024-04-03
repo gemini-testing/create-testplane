a09af8760a7adb6498e70385e6cb2c51f3acd2ac
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2024-04-03 17:14:26 +0300

    feat: use export default in typescript config

--- __fixtures/config/withEverything.js
+++ __fixtures/config/withEverything.js
@@ -3,8 +3,7 @@ import path from "path";
 
 const isCi = Boolean(process.env.CI);
 
-// @ts-ignore
-export = {
+export default {
     gridUrl: "http://localhost:4444/wd/hub",
     baseUrl: "http://localhost",
     pageLoadTimeout: 0,
