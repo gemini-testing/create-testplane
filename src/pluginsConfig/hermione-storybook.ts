e4bfbe5ce0f776a1cc7a3513435f5b7375b65491
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2024-04-05 15:41:12 +0300

    fix: use hermione-storybook@1 config

--- src/pluginsConfig/hermione-storybook.ts
+++ src/pluginsConfig/hermione-storybook.ts
@@ -5,9 +5,13 @@ export default {
     name: HERMIONE_STORYBOOK,
     fn: (config: HermioneConfig): void => {
         config.plugins![HERMIONE_STORYBOOK] = {
-            __comment: "https://github.com/gemini-testing/hermione-storybook",
+            __comment1: "https://github.com/gemini-testing/hermione-storybook",
+            __comment2: "To run hermione storybook tests, use --storybook flag when launching hermione",
+            __comment3: "And dont forget to set 'buildStoriesJson: true' in storybook config if you use storybook@6",
             enabled: true,
-            storybookUrl: "http://localhost:6006",
+            autoScreenshots: true,
+            storybookConfigDir: ".storybook",
+            localport: 6006,
         };
     },
 };
